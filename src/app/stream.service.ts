a0b7bd15836c5c931b355d981a97eff81212f26c
ImedZeiri <imedzairi1996@gmail.com>
2025-08-05 18:34:15 +0100

    update code

--- src/app/stream.service.ts
+++ src/app/stream.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 export interface StreamData {
   scoreUrl: string;
@@ -14,17 +16,24 @@ export interface StreamData {
   providedIn: 'root',
 })
 export class StreamService {
+  constructor(private http: HttpClient) {}
+
   getStreams(): Observable<StreamData> {
     const fakeData: StreamData = {
       scoreUrl: '',
       NowPlaying: true,
       streamingUrl:
         'https://e765432.xyz/static/dfc5b4a8b3793cb1176ab4a56dfb3e896ed419aa/getdata.php?chid=7112&ip=197.3.35.24',
-      link: 'https://11gref-1en.com/7112/index.m3u8',
+      link: 'https://11gref-1en.com/7113/index.m3u8',
       Channel: '7112',
       isStatic: false,
     };
 
     return of(fakeData);
   }
+
+  getStreamLink(eventId: number): Observable<Response[]> {
+    const url = `/api/list/listCasinoProduct?eventId=${eventId}`;
+    return this.http.get<Response[]>(url);
+  }  
 }
